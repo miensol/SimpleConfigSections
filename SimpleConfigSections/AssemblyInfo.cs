750ffb8d7f93e6f9c604c8f491c65294a92ddb3f
Piotr Mionskowski <piotr.mionskowski@gmail.com>
2011-12-16 20:05:32 +0100

    updated assembly info

--- SimpleConfigSections/AssemblyInfo.cs
+++ SimpleConfigSections/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SimpleConfigSections")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Simplifies defining .net configuration sections")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SimpleConfigSections")]
