e4ac065308c2a1299102bda51b62b036a82a0325
Piotr Mionskowski <piotr.mionskowski@gmail.com>
2011-12-17 14:14:30 +0100

    added caching utilities

---
+++ SimpleConfigSections/CacheCallback.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SimpleConfigSections
+{
+    internal class CacheCallback<TKey,TValue>
+        where TValue : class 
+    {
+        private readonly Func<TKey, TValue> _valueResolver;
+        private readonly Cache<TKey, TValue> _cache = new Cache<TKey, TValue>();
+
+        public CacheCallback(Func<TKey,TValue> valueResolver)
+        {
+            _valueResolver = valueResolver;
+        }
+
+        public TValue Get(TKey key)
+        {
+            TValue value = _cache.Get(key);
+            if(value == null)
+            {
+                value = _valueResolver(key);
+                _cache.Insert(key, value, CacheStrategy.Permanent);
+            }
+            return value;
+        }
+    }
+}
