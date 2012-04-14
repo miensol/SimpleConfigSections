4ce79dd3ac27334d04a87d33b7f8c509202f51c5
Piotr Mionskowski <piotr.mionskowski@gmail.com>
2012-04-14 17:19:01 +0200

    fixed project file

--- SimpleConfigSections/ConfigurationPropertyFactory.cs
+++ SimpleConfigSections/ConfigurationPropertyFactory.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Configuration;
@@ -100,30 +99,6 @@ namespace SimpleConfigSections
         private static Action<ConfigurationProperty, string> CreateFieldSetterDelegate(string fieldName)
         {
             return typeof (ConfigurationProperty).MakeSetterForPrivateField<ConfigurationProperty, string>(fieldName);
-        }
-    }
-
-    internal class ConfigurationElementHiddenPropertyBagModifier
-    {
-        private static Hashtable PropertyBagAccessor = CreatePropertyBagAccessor();
-
-        private static Hashtable CreatePropertyBagAccessor()
-        {
-            var propertyBagField = typeof (ConfigurationElement).GetField("s_propertyBags",
-                                                                          BindingFlags.Static | BindingFlags.NonPublic);
-            var value = (Hashtable)propertyBagField.GetValue(null);
-            return value;
-        }
-
-        public void AddConfigurationProperty(ConfigurationProperty configurationProperty, Type ownerType)
-        {
-            var properties = (System.Configuration.ConfigurationPropertyCollection)PropertyBagAccessor[ownerType]; 
-            if(properties == null)
-            {
-                properties = new System.Configuration.ConfigurationPropertyCollection();
-                PropertyBagAccessor[ownerType] = properties;
-            }
-            properties.Add(configurationProperty);
         }
     }
 }
